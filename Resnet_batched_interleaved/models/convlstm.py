f4de44c6a0899b6734faa317cbc2d2953e4bc5db
Varun Jain <varun.jain@iitgn.ac.in>
2021-05-11 23:11:11 +0530

    minor change

--- Resnet_batched_interleaved/models/convlstm.py
+++ Resnet_batched_interleaved/models/convlstm.py
@@ -142,9 +142,7 @@ class ConvLSTM(nn.Module):
         b, _, _, h, w = input_tensor.size()
 
         # Implement stateful ConvLSTM
-        if hidden_state is not None:
-            raise NotImplementedError()
-        else:
+        if hidden_state is None:
             # Since the init is done in forward. Can send image size here
             hidden_state = self._init_hidden(batch_size=b,
                                              image_size=(h, w))
