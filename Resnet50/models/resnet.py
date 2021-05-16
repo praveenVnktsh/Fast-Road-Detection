890100dcb91bc2fdd94c79ba403ec7d156b3bd39
Varun Jain <varun.jain@iitgn.ac.in>
2021-05-16 22:34:57 +0530

    added resnet50

---
+++ Resnet50/models/resnet.py
@@ -0,0 +1,57 @@
+from __future__ import print_function
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torchvision import models
+from torch.optim.rmsprop import RMSprop
+
+
+class Resnet(nn.Module):
+    def __init__(self, pretrained=True, model='resnet18', requires_grad=False, show_params=False):
+        super(Resnet, self).__init__()
+
+        self.model = None
+        if model == 'resnet18':
+            self.model = models.resnet18(pretrained=pretrained)
+
+        elif model == 'resnet101':
+            self.model = models.resnet101(pretrained=pretrained)
+
+        elif model == 'resnet50':
+            self.model = models.resnet50(pretrained=pretrained)
+
+        modules = list(self.model.children())[:-2]
+        self.model = nn.Sequential(*modules)
+
+        for p in self.model.parameters():
+            p.requires_grad = requires_grad
+
+        if model == 'resnet101' or model == 'resnet50':
+            self.model = nn.Sequential(
+                self.model,
+                nn.Conv2d(2048, 512, 3, 1, 1)
+            )
+
+        if show_params:
+            for name, param in self.named_parameters():
+                print(name, param.size())
+
+    def forward(self, x):
+        output = {}
+
+        output["x5"] = self.model(x)
+
+        return output
+
+
+if __name__ == "__main__":
+    resnet18 = Resnet(model='resnet18')
+    resnet101 = Resnet(model='resnet101')
+
+    x = torch.randn(1, 3, 160, 160)
+    print(resnet18)
+    print(resnet101)
+    print(resnet18(x)['x5'].size())
+    print(resnet101(x)['x5'].size())
